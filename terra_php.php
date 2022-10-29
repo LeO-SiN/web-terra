d1b5dc8829aefacacee75bc433d043a3fb2b791d
LeO-SiN <96896564+LeO-SiN@users.noreply.github.com>
2022-10-29 17:24:34 +0530

    Update terra_php.php

--- terra_php.php
+++ terra_php.php
@@ -2,9 +2,9 @@
 <h1>THIS IS MY AMAZON WEBSITE WORKING ON CLOUD</h1>
 
 <video width="750" height="590" controls>
-  <source src="https://aws-2034.s3.amazonaws.com/videoplayback.mp4" type="video/mp4">
+  <source src="https://aws-2034.s3.us-east-1.amazonaws.com/videoplayback.mp4" type="video/mp4">
 </video>
-<img src='https://aws-2034.s3.amazonaws.com/download.jpg' height=560 width=550 />
+<img src='https://aws-2034.s3.us-east-1.amazonaws.com/download.jpg' height=560 width=550 />
 
 <pre>
 
